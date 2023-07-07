a6999740eedde62a9a5dcc65fc9cac13610bbec3
hydou-issei <riasgremory.dxd2022@gmail.com>
2023-07-07 14:28:54 +0630

    complete

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let loginForm = document.querySelector('.login-form_container')
 let formClose = document.querySelector('#form-close')
 let menu = document.querySelector('#menu-bar')
 let navbar = document.querySelector('.nav-bar')
+let videoBtn = document.querySelectorAll('.vid-btn')
 
 
 window.onscroll = () => {
@@ -28,3 +29,53 @@ formBtn.addEventListener('click', () => {
 formClose.addEventListener('click', () => {
     loginForm.classList.remove('active');
 })
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.control .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src
+    });
+});
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 5,
+        },
+    },
+});
+
